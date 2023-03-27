03faa3d1484efee5695d2bc3ae75bc6f0bc87e33
Lars Veldscholte <lars@tuxplace.nl>
2023-03-27 11:32:08 +0200

    Abstract integrators and add velocity-verlet

---
+++ src/integrators/velocity_verlet.rs
@@ -0,0 +1,33 @@
+use cgmath::prelude::*;
+
+use crate::integrators::Integrator;
+use crate::utility::image;
+use crate::{BOX_SIZE, TIMESTEP};
+use crate::types::Particle;
+
+/// velocity Verlet integrators
+pub struct VelocityVerlet;
+
+impl Integrator for VelocityVerlet {
+    fn integrate_a(p: &mut Particle) {
+        p.velocity += 0.5 * p.force * TIMESTEP;
+        p.position += p.velocity * TIMESTEP;
+    }
+
+    fn integrate_b(p: &mut Particle, limit: Option<f64>) {
+        p.velocity += 0.5 * p.force * TIMESTEP;
+
+        // Limit velocity
+        if limit.is_some() {
+            let speed= limit.unwrap();
+
+            if p.velocity.magnitude2() > speed*speed {
+                p.velocity = p.velocity.normalize_to(speed);
+            }
+        }
+
+        // Apply PBCs
+        let image = image(p.position, BOX_SIZE);
+        p.position -= BOX_SIZE*image;
+    }
+}
---
+++ src/integrators/verlet.rs
@@ -0,0 +1,38 @@
+use cgmath::prelude::*;
+use cgmath::Point3;
+
+use crate::integrators::Integrator;
+use crate::utility::image;
+use crate::{BOX_SIZE, TIMESTEP};
+use crate::types::Particle;
+
+/// Störmer–Verlet integrator
+pub struct Verlet;
+
+impl Integrator for Verlet {
+    fn integrate_a(p: &mut Particle) {}
+
+    fn integrate_b(p: &mut Particle, limit: Option<f64>) {
+        let mut new_position: Point3<f64> = EuclideanSpace::from_vec(2.0*p.position - p.old_position + p.force*TIMESTEP*TIMESTEP);
+        p.velocity = (p.position - p.old_position) / (2.0*TIMESTEP);
+
+        // Limit velocity
+        if limit.is_some() {
+            let speed= limit.unwrap();
+
+            if p.velocity.magnitude2() > speed*speed {
+                p.velocity = p.velocity.normalize_to(speed);
+                new_position = p.old_position + 2.0*p.velocity*TIMESTEP;
+            }
+        }
+
+        // Advance a timestep
+        p.old_position = p.position;
+        p.position = new_position;
+
+        // Apply PBCs
+        let image = image(p.position, BOX_SIZE);
+        p.position -= BOX_SIZE*image;
+        p.old_position -= BOX_SIZE*image;
+    }
+}
